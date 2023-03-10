56e74e30c87af07bef95ef9cd249a62437a434a5
gmatbu <109977431+gmatbu@users.noreply.github.com>
2023-03-10 13:52:08 -0800

    hw 5-6

---
+++ HW6/FoodApp/FoodApp/FoodItem.swift
@@ -0,0 +1,23 @@
+//
+//  FoodItem.swift
+//  FoodApp
+//
+//  Created by Larry Holder on 2/12/20.
+//  Copyright © 2020 Washington State University. All rights reserved.
+//
+
+import Foundation
+
+class FoodItem {
+    var name: String
+    var imageFileName: String
+    
+    init(name: String, imageFileName: String) {
+        
+        
+        self.name = name
+        self.imageFileName = imageFileName
+        
+        
+    }
+}
